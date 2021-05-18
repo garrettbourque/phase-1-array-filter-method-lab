2052eadbbec25f3897ff6dde4a476c588c50b8a4
garrettBourque <bourque.garrett@gmail.com>
2021-05-18 18:05:07 -0500

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+function findMatching(drivers,name){
+    return drivers.filter(drivers=>drivers===name||drivers.toUpperCase()===name.toUpperCase());
+}
+
+function fuzzyMatch(drivers, start){
+    return drivers.filter(drivers=>drivers.startsWith(start)||drivers.toUpperCase().startsWith(start.toUpperCase()));
+
+}
+
+function matchName(drivers, name){
+    return drivers.filter(drivers=>drivers.name===name||drivers.name.toUpperCase()===name.toUpperCase());
+
+}
